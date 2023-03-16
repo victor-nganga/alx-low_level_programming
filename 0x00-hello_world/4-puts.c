442bb18d74f654eaff9a7e6e5883915673748d3d
victor-nganga <ngashvictor61@gmailcom>
2023-03-16 15:57:01 +0300

    mint

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
+#!/bin/bash
 #include <stdio.h>
-
+/**
+ * main - main entry point
+ * return : always 0 (succed)
+ */
 int main(void)
 {
 	puts (""Programming is like building a multilingual puzzle");
