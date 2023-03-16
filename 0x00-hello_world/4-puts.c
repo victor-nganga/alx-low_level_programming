8d433b58e1e016105a3ee11d09fc4bfbbf5e720f
victor-nganga <ngashvictor61@gmailcom>
2023-03-16 16:33:00 +0300

    brew

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 /**
- * main -main entry point
- * return : always 0 (success)
+ * main - main entry point
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
+
