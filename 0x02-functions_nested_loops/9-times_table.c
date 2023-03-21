6f9f08ea37af0552c2e6f668b750aef564afdf87
victor-nganga <ngashvictor61@gmailcom>
2023-03-21 16:24:12 +0300

    timetable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int i, j, res;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0');
+		for (j = 1; j < 10; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			res = i * j;
+			if (res < 10)
+			{
+				_putchar(' ');
+				_putchar(res + '0');
+			}
+			else
+			{
+				_putchar(res / 10 + '0');
+				_putchar(res % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
