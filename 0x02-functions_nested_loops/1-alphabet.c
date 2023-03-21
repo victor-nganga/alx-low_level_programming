e7f85fcddd8ea4643d43928f2b72ea95867e1583
victor-nganga <ngashvictor61@gmailcom>
2023-03-21 12:24:53 +0300

    added tasks 0 through 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/*
+ * main - Entry point
+ * prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return: Always (0)
+ */
+
+void print_alphabet(void)
+{
+	int i;
+	for (i = 97; i <== 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <main.h>
+/**
+ * main - Main entry point
+ *
+ * print_alphabet_x10 - Prints the alphabet 10 times.
+ *
+ * Return: 0 (Success)
+ */
+ 
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (j = 1; i <= 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
+
