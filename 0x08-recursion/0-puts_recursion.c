dc95cde927ed780abac388edbf1aa04a610b3569
victor-nganga <ngashvictor61@gmailcom>
2023-04-05 14:17:29 +0300

    task #0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string followed by a newline
+ * @s: string
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,7 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+void _print_rev_recursion(char *s);
+void _puts_recursion(char *s);
+
+#endif
